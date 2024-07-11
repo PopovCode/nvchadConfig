3999a3c1ceef7bd780fe4a269d3777fe55c4e546
Антон Попов <popov@MacBook-Air-Anton.local>
2024-07-11 16:30:54 +0300

    init

---
+++ lua/mappings.lua
@@ -0,0 +1,76 @@
+require "nvchad.mappings"
+
+local map = vim.keymap.set
+
+map("n", ";", ":", { desc = "CMD enter command mode" })
+map("i", "jj", "<ESC>")
+map("i", "оо", "<ESC>")
+map("i", "<C-g>", function()
+  return vim.fn["codeium#Accept"]()
+end, { expr = true })
+
+map("n", ";", ":", { desc = "CMD enter command mode" })
+map("n", "<leader>w", "<cmd>w<CR>", { desc = "Save" })
+map("n", "<leader>cx", function()
+  require("nvchad.tabufline").closeAllBufs()
+end, { desc = "Close All Buffers" })
+
+-- Debug
+map("n", "<leader>du", function()
+  require("dapui").toggle()
+end, { desc = "Toggle Debug UI" })
+map("n", "<leader>db", function()
+  require("dap").toggle_breakpoint()
+end, { desc = "Toggle Breakpoint" })
+map("n", "<leader>ds", function()
+  require("dap").continue()
+end, { desc = "Start" })
+map("n", "<leader>dn", function()
+  require("dap").step_over()
+end, { desc = "Step Over" })
+
+--NvimTree
+map("n", "<leader>e", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle NvimTree" })
+
+-- TODO
+
+map("n", "<leader>ft", "<cmd>TodoTelescope<CR>", { desc = "Find Todo" })
+
+-- SymbolsOutline
+
+map("n", "<leader>fs", "<cmd>SymbolsOutline<CR>", { desc = "Toggle SymbolsOutline" })
+
+--Split/Resize Buffers
+
+map("n", "\\", "<cmd>:vsplit <CR>", { desc = "Vertical Split" })
+map("n", "<leader>[", "<cmd>:vertical resize -10 <CR>", { desc = "Vertical +" })
+map("n", "<leader>]", "<cmd>:vertical resize +10 <CR>", { desc = "Vertical -" })
+
+-- Trouble
+
+map("n", "<leader>td", "<cmd>Trouble diagnostics toggle<CR>", { desc = "Open Trouble diagnostics" })
+-- map("n", "<leader>qw", "<cmd>TroubleToggle workspace_diagnostics<CR>", { desc = "Open Workspace Trouble" })
+-- map("n", "<leader>qd", "<cmd>TroubleToggle document_diagnostics<CR>", { desc = "Open Document Trouble" })
+-- map("n", "<leader>qq", "<cmd>TroubleToggle quickfix<CR>", { desc = "Open Quickfix" })
+-- map("n", "<leader>ql", "<cmd>TroubleToggle loclist<CR>", { desc = "Open Location List" })
+
+-- Terminal
+
+map("n", "<C-]>", function()
+  require("nvchad.term").toggle { pos = "vsp", size = 0.4 }
+end, { desc = "Toogle Terminal Vertical" })
+map("n", "<C-\\>", function()
+  require("nvchad.term").toggle { pos = "sp", size = 0.4 }
+end, { desc = "Toogle Terminal Horizontal" })
+map("n", "<C-f>", function()
+  require("nvchad.term").toggle { pos = "float" }
+end, { desc = "Toogle Terminal Float" })
+map("t", "<C-]>", function()
+  require("nvchad.term").toggle { pos = "vsp" }
+end, { desc = "Toogle Terminal Vertical" })
+map("t", "<C-\\>", function()
+  require("nvchad.term").toggle { pos = "sp" }
+end, { desc = "Toogle Terminal Horizontal" })
+map("t", "<C-f>", function()
+  require("nvchad.term").toggle { pos = "float" }
+end, { desc = "Toogle Terminal Float" })
---
+++ lua/plugins/init.lua
@@ -0,0 +1,225 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    event = "BufWritePre", -- uncomment for format on save
+    config = function()
+      require "configs.conform"
+    end,
+  },
+  {
+    "stevearc/dressing.nvim",
+    lazy = false,
+    opts = {},
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require("nvchad.configs.lspconfig").defaults()
+      require "configs.lspconfig"
+    end,
+  },
+  { "nvim-neotest/nvim-nio" },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "lua-language-server",
+        "stylua",
+        "html-lsp",
+        "css-lsp",
+        "prettier",
+        "eslint-lsp",
+        "js-debug-adapter",
+        "typescript-language-server",
+        "eslint_d",
+        "prisma-language-server",
+      },
+    },
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "vim",
+        "lua",
+        "vimdoc",
+        "html",
+        "css",
+        "typescript",
+        "javascript",
+        "sql",
+      },
+    },
+  },
+  {
+    "mfussenegger/nvim-lint",
+    event = "VeryLazy",
+    config = function()
+      require "configs.lint"
+    end,
+  },
+  {
+    "windwp/nvim-ts-autotag",
+    event = "VeryLazy",
+    config = function()
+      require("nvim-ts-autotag").setup()
+    end,
+  },
+  {
+    "max397574/better-escape.nvim",
+    event = "InsertEnter",
+    config = function()
+      require("better_escape").setup()
+    end,
+  },
+  {
+    "ggandor/leap.nvim",
+    lazy = false,
+    config = function()
+      require("leap").add_default_mappings(true)
+    end,
+  },
+  {
+    "folke/trouble.nvim",
+    lazy = false,
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      require("trouble").setup()
+    end,
+  },
+  {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    lazy = false,
+    config = function()
+      require("todo-comments").setup()
+    end,
+  }, -- To make a plugin not be loaded
+  {
+    "Exafunction/codeium.vim",
+    lazy = false,
+  },
+  {
+    "axelvc/template-string.nvim",
+    lazy = false,
+    config = function()
+      require("template-string").setup {
+        filetypes = {
+          "html",
+          "typescript",
+          "javascript",
+          "typescriptreact",
+          "javascriptreact",
+          "vue",
+          "svelte",
+          "python",
+        }, -- filetypes where the plugin is active
+        jsx_brackets = true, -- must add brackets to JSX attributes
+        remove_template_string = false, -- remove backticks when there are no template strings
+        restore_quotes = {
+          -- quotes used when "remove_template_string" option is enabled
+          normal = [[']],
+          jsx = [["]],
+        },
+      }
+    end,
+  },
+  {
+    "folke/neodev.nvim",
+    lazy = false,
+    config = function()
+      require("neodev").setup()
+    end,
+  },
+  {
+    "prisma/vim-prisma",
+  },
+  {
+    "simrat39/symbols-outline.nvim",
+    lazy = false,
+    config = function()
+      require("symbols-outline").setup()
+    end,
+  },
+  {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    config = function()
+      require "configs.noice"
+    end,
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+    },
+  },
+
+  -- For Noice dependencies
+  {
+    "rcarriga/nvim-notify",
+    config = function()
+      require("notify").setup {
+        background_colour = "#1e1e1e",
+      }
+    end,
+  },
+  {
+    "mfussenegger/nvim-dap",
+    config = function()
+      local ok, dap = pcall(require, "dap")
+      if not ok then
+        return
+      end
+      dap.configurations.typescript = {
+        {
+          type = "node2",
+          name = "node attach",
+          request = "attach",
+          program = "${file}",
+          cwd = vim.fn.getcwd(),
+          sourceMaps = true,
+          protocol = "inspector",
+        },
+      }
+
+      dap.configurations.javascript = {
+        {
+          type = "node2",
+          name = "node attach",
+          request = "attach",
+          program = "${file}",
+          cwd = vim.fn.getcwd(),
+          sourceMaps = true,
+          protocol = "inspector",
+        },
+      }
+      dap.adapters.node2 = {
+        type = "executable",
+        command = "node-debug2-adapter",
+        args = {},
+      }
+    end,
+    dependencies = {
+      "mxsdev/nvim-dap-vscode-js",
+    },
+  },
+  {
+    "rcarriga/nvim-dap-ui",
+    config = function()
+      require("dapui").setup()
+
+      local dap, dapui = require "dap", require "dapui"
+
+      dap.listeners.after.event_initialized["dapui_config"] = function()
+        dapui.open {}
+      end
+      dap.listeners.before.event_terminated["dapui_config"] = function()
+        dapui.close {}
+      end
+      dap.listeners.before.event_exited["dapui_config"] = function()
+        dapui.close {}
+      end
+    end,
+    dependencies = {
+      "mfussenegger/nvim-dap",
+    },
+  },
+}
